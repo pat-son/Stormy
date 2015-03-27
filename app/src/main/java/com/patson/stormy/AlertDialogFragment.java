aabf1738144ab73973666f784b85221051c4eb95
pat-son <patrick.son@comcast.net>
2015-03-26 23:16:22 -0400

    Added stuff

---
+++ app/src/main/java/com/patson/stormy/AlertDialogFragment.java
@@ -0,0 +1,23 @@
+package com.patson.stormy;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Copyright 2015 Patrick Son
+ */
+public class AlertDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context) ;
+        builder.setTitle(context.getString(R.string.error_title))
+                .setMessage(context.getString(R.string.error_message))
+                .setPositiveButton(context.getString(R.string.error_okay), null) ;
+
+        return builder.create();
+    }
+}
--- app/src/main/java/com/patson/stormy/MainActivity.java
+++ app/src/main/java/com/patson/stormy/MainActivity.java
@@ -1,9 +1,13 @@
 package com.patson.stormy;
 
 //import android.app.DownloadManager;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
@@ -29,30 +33,53 @@ public class MainActivity extends ActionBarActivity {
         String forecastUrl = "https://api.forecast.io/forecast/" + apiKey +
                 "/" + latitude + "," + longitude ;
 
-        OkHttpClient client = new OkHttpClient() ;
-        Request request = new Request.Builder().url(forecastUrl).build();
+        if( isNetworkIsAvailable()) {
 
-        Call call = client.newCall(request);
-        call.enqueue( new Callback() {
-            @Override
-            public void onFailure(Request request, IOException e) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(forecastUrl).build();
 
-            }
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
 
-            @Override
-            public void onResponse(Response response) throws IOException {
-                try {
-                    if(response.isSuccessful()) {
-                        Log.v(TAG, response.body().string());
-                    }
-                } catch (IOException e) {
-                    Log.e(TAG, "Exception caught: ", e);
                 }
-            }
-        });
+
+                @Override
+                public void onResponse(Response response) throws IOException {
+                    try {
+                        Log.v(TAG, response.body().string());
+                        if (response.isSuccessful()) {
+
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
+                }
+            });
+        }
+        else {
+            Toast.makeText(this, getString(R.string.network_unavailable_message), Toast.LENGTH_LONG).show() ;
+        }
 
         Log.d(TAG, "Main UI Code is running.");
 
+    }
+
+    private boolean isNetworkIsAvailable() {
+        ConnectivityManager manager =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false ;
+        if( networkInfo != null && networkInfo.isConnected() ) isAvailable = true ;
+        return isAvailable;
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 
 }
