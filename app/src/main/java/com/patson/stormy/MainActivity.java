88d1556d29689f37f75320593e93c9a48c199819
pat-son <patrick.son@comcast.net>
2015-03-26 22:32:33 -0400

    Initial commit

---
+++ app/src/main/java/com/patson/stormy/MainActivity.java
@@ -0,0 +1,58 @@
+package com.patson.stormy;
+
+//import android.app.DownloadManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String apiKey = "5b2d7a04d5d9d3c4fa273a63a589e297";
+        double latitude = 37.8267;
+        double longitude = -122.423;
+        String forecastUrl = "https://api.forecast.io/forecast/" + apiKey +
+                "/" + latitude + "," + longitude ;
+
+        OkHttpClient client = new OkHttpClient() ;
+        Request request = new Request.Builder().url(forecastUrl).build();
+
+        Call call = client.newCall(request);
+        call.enqueue( new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                try {
+                    if(response.isSuccessful()) {
+                        Log.v(TAG, response.body().string());
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Exception caught: ", e);
+                }
+            }
+        });
+
+        Log.d(TAG, "Main UI Code is running.");
+
+    }
+
+}
